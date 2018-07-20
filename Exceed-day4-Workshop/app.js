526162fc883dd857f18e4c17dbe9adfb26bcb780
Vichakorn Yotboonrueang <viyotboon_new@outlook.com>
2018-07-20 09:46:51 +0700

    Stater

---
+++ Exceed-day4-Workshop/app.js
@@ -0,0 +1,11 @@
+$(function () {
+    $.ajax({
+        type: "GET",
+        url: "http://ecourse.cpe.ku.ac.th:1515/api/odin-plam/view",
+        data: "data",
+        dataType: "dataType",
+        success: function (response) {
+            
+        }
+    });
+})
