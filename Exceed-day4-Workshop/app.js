589c0ff34c8b4a206d417a56501b7e0820c962a5
Kornphon Noiprasert <konphon22@hotmail.com>
2018-07-20 12:42:42 +0700

    On-off button works

--- Exceed-day4-Workshop/app.js
+++ Exceed-day4-Workshop/app.js
@@ -8,4 +8,32 @@ $(function () {
             
         }
     });
+
+    $('#off-light-button').on('click', function () {
+        $('#light').html('<img width="200" src="https://www.flaticon.com/premium-icon/icons/svg/595/595136.svg" alt="" class="rounded">')
+    })
+    $('#on-light-button').on('click', function () {
+        $('#light').html('<img width="200" src="https://www.flaticon.com/premium-icon/icons/svg/595/595085.svg" alt="" class="rounded">')
+    })
+
+    $('#off-door-button').on('click', function () {
+        $('#door').html('<img width="200" src="https://image.flaticon.com/icons/svg/876/876904.svg" alt="" class="rounded">')
+    })
+    $('#on-door-button').on('click', function () {
+        $('#door').html('<img width="200" src="https://image.flaticon.com/icons/svg/876/876853.svg" alt="" class="rounded">')
+    })
+
+    $('#off-air-button').on('click', function () {
+        $('#air').html('<img width="200" src="https://www.flaticon.com/premium-icon/icons/svg/701/701181.svg" alt="" class="rounded">')
+    })
+    $('#on-air-button').on('click', function () {
+        $('#air').html('<img width="200" src="https://image.flaticon.com/icons/svg/701/701110.svg" alt="" class="rounded">')
+    })
+
+    $('#off-alarm-button').on('click', function () {
+        $('#alarm').html('<img width="200" src="https://image.flaticon.com/icons/svg/883/883396.svg" alt="" class="rounded">')
+    })
+    $('#on-alarm-button').on('click', function () {
+        $('#alarm').html('<img width="200" src="https://image.flaticon.com/icons/svg/883/883345.svg" alt="" class="rounded">')
+    })
 })
